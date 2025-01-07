2e0e9cbfa36300c3ad8bdae56c87a8bd2c95a460
sreeja0616 <ssreejabeerkula@gmail.com>
2025-01-07 15:16:48 +0530

    message

---
+++ Day5_loops.java
@@ -0,0 +1,18 @@
+public class Day5_loops{
+    public static void main(Strings[] args) {
+        System.out.println(x:"hello");
+       // count =0
+       //count = 1
+       //4
+       // count = 2
+       // count = 3
+       // count = 4
+       // loop
+       // 1. initialization
+       //2. incrementation/decrement
+       for(int count = 20; count <= 35;count++){
+        //count1
+        System.out.println(count);
+       } 
+    }
+}
